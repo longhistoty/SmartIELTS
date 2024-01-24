918a5bfbd50988fde1a8b873e385769881ea46eb
Elizaveta-And <149873736+Elizaveta-And@users.noreply.github.com>
2024-01-24 16:59:53 +0300

    Add files via upload

--- lib/TestScreen.dart
+++ lib/TestScreen.dart
@@ -2,8 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'MenuScreen.dart';
-///,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-import 'dart:async';
 
 class TestScreen extends StatefulWidget {
   const TestScreen({Key? key}) : super(key: key);
@@ -13,44 +11,8 @@ class TestScreen extends StatefulWidget {
 }
 
 class _TestScreenState extends State<TestScreen> {
-  Duration _duration = Duration(hours :0, minutes: 0, seconds: 3);
-  int _secondsElapsed = 0;
-  Timer? _timer;
-
-  @override
-  void initState() {
-    super.initState();
-    startTimer();
-  }
-
-  void startTimer() {
-    const oneSecond = Duration(seconds: 1);
-    _timer = Timer.periodic(oneSecond, (Timer timer) {
-      setState(() {
-        _duration -= oneSecond;
-        if (_duration.inSeconds <= 0) {
-          stopTimer();
-        }
-      });
-    });
-  }
-
-  void stopTimer() {
-    _timer?.cancel();
-  }
-
-  @override
-  void dispose() {
-    _timer?.cancel();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
-    String twoDigits(int n) => n.toString().padLeft(2, '0');
-    final hours = twoDigits(_duration.inHours.remainder(60));
-    final minutes = twoDigits(_duration.inMinutes.remainder(60));
-    final seconds = twoDigits(_duration.inSeconds.remainder(60));
     Widget _testInfo() {
       return Container(
         padding: EdgeInsets.only(top: 30),
@@ -63,10 +25,7 @@ class _TestScreenState extends State<TestScreen> {
             decoration: BoxDecoration(
               borderRadius: BorderRadius.only(
                   topRight: Radius.circular(70), topLeft: Radius.circular(70)),
-              color:
-              _duration.inSeconds > 0
-                  ? Colors.greenAccent.shade100
-                  : Colors.redAccent.shade100,
+              color: Colors.greenAccent.shade100,
             ),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -84,9 +43,7 @@ class _TestScreenState extends State<TestScreen> {
                   height: 15,
                 ),
                 Text(
-                  _duration.inSeconds > 0
-                      ? "Time: $hours:$minutes:$seconds"
-                      : "DONE",
+                  "Time:",
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 25,
@@ -100,137 +57,195 @@ class _TestScreenState extends State<TestScreen> {
         ),
       );
     }
-///,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+
     Widget _skills() {
       return Container(
-        alignment: Alignment.center,
-        padding: EdgeInsets.symmetric(horizontal: 21),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            for (var row = 0; row < 2; row++)
-              Row(
-                children: [
-                  for (var skill in [
-                    "Speaking",
-                    "Listening",
-                    "Reading",
-                    "Writing"
-                  ].skip(row * 2).take(2))
-                    Container(
-                      width: 165,
-                      height: 100,
-                      margin: EdgeInsets.only(right: 10, bottom: 10),
-                      decoration: BoxDecoration(color: Colors.purple.shade200),
-                      child: TextButton(
-                        onPressed: () {},
-                        child: Text(
-                          skill,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 25,
-                            color: Colors.black,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
+          alignment: Alignment.center,
+          width: 400,
+          height: 250,
+          padding: EdgeInsets.only(left: 21, right: 13),
+          child: GridView.count(
+            crossAxisCount: 2,
+            crossAxisSpacing: 5.0,
+            mainAxisSpacing: 5.0,
+            childAspectRatio: 1.5,
+            children: [
+              Container(
+                width: 165,
+                height: 100,
+                margin: EdgeInsets.only(right: 10, bottom: 10),
+                decoration: BoxDecoration(color: Colors.purple.shade200),
+                child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Speaking",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                        color: Colors.black,
                       ),
+                      textAlign: TextAlign.center,
                     ),
-                ],
+                    style: TextButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder())),
               ),
-            Container(
-                padding: EdgeInsets.only(top: 10),
-                child: Align(
-                  alignment: Alignment.topCenter,
-                  child: Container(
-                    width: 350,
-                    height: 90,
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                          bottomRight: Radius.circular(70),
-                          bottomLeft: Radius.circular(70)),
-                      color: Colors.greenAccent.shade100,
+              Container(
+                width: 165,
+                height: 100,
+                margin: EdgeInsets.only(right: 10, bottom: 10),
+                decoration: BoxDecoration(color: Colors.purple.shade200),
+                child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Listening",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                        color: Colors.black,
+                      ),
+                      textAlign: TextAlign.center,
                     ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        TextButton(
-                          onPressed: () {},
-                          child: Text(
-                            "Useful vocabulary",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 25,
-                              color: Colors.black,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ],
+                    style: TextButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder())),
+              ),
+              Container(
+                width: 165,
+                height: 100,
+                margin: EdgeInsets.only(right: 10, bottom: 10),
+                decoration: BoxDecoration(color: Colors.purple.shade200),
+                child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Reading",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                        color: Colors.black,
+                      ),
+                      textAlign: TextAlign.center,
                     ),
+                    style: TextButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder())),
+              ),
+              Container(
+                width: 165,
+                height: 100,
+                margin: EdgeInsets.only(right: 10, bottom: 10),
+                decoration: BoxDecoration(color: Colors.purple.shade200),
+                child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Writing",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                        color: Colors.black,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    style: TextButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder())),
+              ),
+            ],
+          ));
+    }
+
+    Widget _tips() {
+      return Container(
+          child: Align(
+        alignment: Alignment.topCenter,
+        child: Container(
+          width: 350,
+          height: 90,
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(70),
+                bottomLeft: Radius.circular(70)),
+            color: Colors.greenAccent.shade100,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  "Useful vocabulary",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                    color: Colors.black,
                   ),
-                ))
-          ],
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
         ),
-      );
+      ));
     }
 
     return Scaffold(
-        appBar: AppBar(
-          elevation: 10,
-          centerTitle: true,
-          backgroundColor: Colors.purple.shade200,
-          titleSpacing: 40,
-          title: Text("SMART IELTS",
-              style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white)),
-          leading: IconButton(
-              icon: Icon(
-                Icons.more_vert,
-                color: Colors.white,
-                size: 35,
-              ),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    PageRouteBuilder(
-                        pageBuilder: (context, animation, secondaryAnimation) =>
-                            Menu(),
-                        transitionsBuilder:
-                            (context, animation, secondaryAnimation, child) {
-                          const begin = Offset(-1.0, 0.0);
-                          const end = Offset(0.0, 0.0);
-                          const curve = Curves.easeInOut;
-                          var tween = Tween(begin: begin, end: end)
-                              .chain(CurveTween(curve: curve));
-                          var offsetAnimation = animation.drive(tween);
-                          return SlideTransition(
-                              position: offsetAnimation, child: child);
-                        }));
-              }),
-          actions: [
-            Container(
-              padding: EdgeInsets.only(right: 10, left: 0),
-              child: IconButton(
-                icon: Icon(Icons.door_back_door_outlined,
-                    size: 38, color: Colors.white),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              ),
+      appBar: AppBar(
+        elevation: 10,
+        centerTitle: true,
+        backgroundColor: Colors.purple.shade200,
+        titleSpacing: 40,
+        title: Text("SMART IELTS",
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.white)),
+        leading: IconButton(
+            icon: Icon(
+              Icons.more_vert,
+              color: Colors.white,
+              size: 35,
             ),
-          ],
-        ),
-        body: SingleChildScrollView(
-            child: Column(children: <Widget>[
-          _testInfo(),
-          SizedBox(
-            height: 20,
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  PageRouteBuilder(
+                      pageBuilder: (context, animation, secondaryAnimation) =>
+                          Menu(),
+                      transitionsBuilder:
+                          (context, animation, secondaryAnimation, child) {
+                        const begin = Offset(-1.0, 0.0);
+                        const end = Offset(0.0, 0.0);
+                        const curve = Curves.easeInOut;
+                        var tween = Tween(begin: begin, end: end)
+                            .chain(CurveTween(curve: curve));
+                        var offsetAnimation = animation.drive(tween);
+                        return SlideTransition(
+                            position: offsetAnimation, child: child);
+                      }));
+            }),
+        actions: [
+          Container(
+            padding: EdgeInsets.only(right: 10, left: 0),
+            child: IconButton(
+              icon: Icon(Icons.door_back_door_outlined,
+                  size: 38, color: Colors.white),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
           ),
-          _skills(),
-        ])),
+        ],
+      ),
+      body: SingleChildScrollView(
+          child: Column(children: <Widget>[
+        _testInfo(),
+        SizedBox(
+          height: 20,
+        ),
+        _skills(),
+        _tips()
+      ])),
       bottomNavigationBar: BottomAppBar(
         color: Colors.purple.shade200,
         child: Row(
@@ -242,8 +257,7 @@ class _TestScreenState extends State<TestScreen> {
                 color: Colors.white,
                 size: 45,
               ),
-              onPressed: () {
-              },
+              onPressed: () {},
             ),
           ],
         ),
