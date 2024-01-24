6d6b024faad413538de8afc8cbbc9efcd63a98b5
longhistoty <136375077+longhistoty@users.noreply.github.com>
2024-01-24 21:49:58 +0500

    Update main.dart
    auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
 import 'AuthScreen.dart';
+//
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
-  runApp(MyApp());
+import 'screens/account_screen.dart';
+//import 'package:flutter_course/screens/home_screen.dart';
+import 'package:flutter_course/Homescreen.dart';
+//import 'screens/login_screen.dart';
+import 'screens/reset_password_screen.dart';
+import 'screens/signup_screen.dart';
+import 'screens/verify_email_screen.dart';
+import 'services/firebase_streem.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,11 +25,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        pageTransitionsTheme: const PageTransitionsTheme(builders: {
+          TargetPlatform.android: CupertinoPageTransitionsBuilder(),
+        }),
         colorScheme: ColorScheme.fromSwatch()
             .copyWith(background: Colors.white),
         fontFamily: 'Ubuntu',),
-      home: AuthScreen(),
+      //home: const AuthScreen(),
+      routes: {
+        '/': (context) => const FirebaseStream(),
+        '/home': (context) => IeltsSmart(),
+        '/account': (context) => const AccountScreen(),
+        '/login': (context) => const AuthScreen(),
+        '/signup': (context) => const SignUpScreen(),
+        '/reset_password': (context) => const ResetPasswordScreen(),
+        '/verify_email': (context) => const VerifyEmailScreen(),
+      },
+      initialRoute: '/login',
     );
   }
 }
